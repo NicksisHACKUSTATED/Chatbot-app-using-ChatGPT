76d492d770bb063fed1abe6618c40cdc894bf293
NicksisHACKUSTATED <83160316+NicksisHACKUSTATED@users.noreply.github.com>
2023-04-19 11:01:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
+// JavaScript code in script.js
+
+// Get DOM elements
+const userInput = document.getElementById('user-input');
+const sendBtn = document.getElementById('send-btn');
+const chatMessages = document.getElementById('chat-messages');
+
+
+// Add event listener to send button
+sendBtn.addEventListener('click', sendMessage);
+
+// Add event listener to input field for pressing Enter key
+userInput.addEventListener('keypress', function (event) {
+  if (event.keyCode === 13 || event.which === 13) {
+    sendMessage();
+  }
+});
+
+// Function to send a message
 function sendMessage() {
-    // Get user input
-    var userInput = document.getElementById('user-input').value;
-    
-    // Create a new chat bubble element
-    var chatBubble = document.createElement('div');
-    chatBubble.className = 'chat-bubble user-message';
-    chatBubble.textContent = 'User: ' + userInput;
-    
-    // Append chat bubble to chat log
-    document.getElementById('chat-log').appendChild(chatBubble);
-    
-    // Call the Reply() function to get bot's response
-    Reply(userInput);
-    
+  const userMessage = userInput.value;
+  if (userMessage.trim() !== '') {
+    // Create a new chat bubble element for user's message
+    const chatBubble = document.createElement('div');
+    chatBubble.className = 'chat-bubble user';
+    chatBubble.textContent = 'User: ' + userMessage;
+    chatMessages.appendChild(chatBubble);
+
     // Clear input field
-    document.getElementById('user-input').value = '';
-    
-    // Scroll to the bottom of the chat log
-    var chatLog = document.getElementById('chat-log');
-    chatLog.scrollTop = chatLog.scrollHeight;
+    userInput.value = '';
+
+    // Scroll to the bottom of the chat messages
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+
+    // Call the chatbot's response function
+    Reply(userMessage);
+  }
 }
 
-function Reply(userInput) {
+// Function for chatbot's response
+function Reply(userMessage) {
     const options = {
         method: 'POST',
         headers: {
@@ -29,7 +46,7 @@ function Reply(userInput) {
             'X-RapidAPI-Key': 'c62bd187demsh0187f521cb4cca9p11f6bejsn544a2ed1f189',
             'X-RapidAPI-Host': 'chatgpt-api7.p.rapidapi.com'
         },
-        body: JSON.stringify({"query": userInput})
+        body: JSON.stringify({"query": userMessage})
     };
     
     fetch('https://chatgpt-api7.p.rapidapi.com/ask', options)
@@ -44,11 +61,11 @@ function Reply(userInput) {
             }
             else{
                 console.log(response);
-                var botInput = response.response; // Update this with the appropriate property from the API response
+                var botInput = response.response; 
                 var chatBubble = document.createElement('div');
                 chatBubble.className = 'chat-bubble bot-message';
                 chatBubble.textContent = 'Bot: ' + botInput; 
-                document.getElementById('chat-log').appendChild(chatBubble);
+                document.getElementById('chat-messages').appendChild(chatBubble);
             }
         })
         .catch(err => console.error(err));
